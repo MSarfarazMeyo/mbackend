ac6251e790a1ddab9a06f5bb2a7246e7a347536c
Muhammad Sarfaraz <msarfaraz.meyo@gmail.com>
2022-11-14 19:14:58 +0500

    port update

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 5000;
 
 // Middlewares
 app.use(express.json());
@@ -21,6 +20,5 @@ app.use("/api", router); // localhost:5000/appdata
 app.use(express.static(__dirname + "/public"));
 app.use("/logos", express.static("logos"));
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`server start at port no ${port}`);
+ app.listen(process.env.PORT || 5000, () => {
 });
